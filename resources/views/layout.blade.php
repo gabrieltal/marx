640e6f2a2a56cb5bcf2bf97c08574db706cb1bad
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-21 10:18:35 -0700

    Moved the header/nav to the layout page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,10 +8,22 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="css/app.css" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
-        @yield ('header')
+        <div id="header-wrapper" class="d-flex align-items-center">
+            <div id="header">
+                <h1><a href="/">Marx</a></h1>
+            </div>
+            <div class="nav">
+                <ul class="d-flex justify-content-between">
+                    <li class="{{ Request::path() === '/' ? 'active-nav-item' : '' }}"><a href="/">Home</a></li>
+                    <li class="{{ Request::path() === 'about' ? 'active-nav-item' : '' }}"><a href="/about">About</a></li>
+                    <li class="{{ Request::path() === 'posts' ? 'active-nav-item' : '' }}"><a href="/posts">Posts</a></li>
+                </ul>
+            </div>
+        </div>
+
         @yield ('content')
 
         <footer><div class="d-flex align-items-center h-100">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,5 @@
 @extends ('layout')
 
-@section ('header')
-    <div id="header-wrapper" class="d-flex align-items-center">
-        <div id="header">
-            <h1><a href="/">Marx</a></h1>
-        </div>
-        <div class="nav">
-            <ul class="d-flex justify-content-between">
-                <li class="{{ Request::path() === '/' ? 'active-nav-item' : '' }}"><a href="/">Home</a></li>
-                <li class="{{ Request::path() === 'about' ? 'active-nav-item' : '' }}"><a href="/about">About</a></li>
-            </ul>
-        </div>
-    </div>
-@endsection
-
 @section ('content')
     <div class="flex-center position-ref full-height">
         @if (Route::has('login'))
