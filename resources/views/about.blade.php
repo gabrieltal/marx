18950b4b6cb9bab390ea267d1d975ffbf07e4746
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-21 10:18:24 -0700

    Added karl marx image to the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,10 @@
 @extends ('layout')
 
-@section ('header')
-    <div id="header-wrapper" class="d-flex align-items-center">
-        <div id="header">
-            <h1><a href="/">Marx</a></h1>
-        </div>
-        <div class="nav">
-            <ul class="d-flex justify-content-between">
-                <li class="{{ Request::path() === '/' ? 'active-nav-item' : '' }}"><a href="/">Home</a></li>
-                <li class="{{ Request::path() === 'about' ? 'active-nav-item' : '' }}"><a href="/about">About</a></li>
-            </ul>
-        </div>
-    </div>
-@endsection
-
 @section ('content')
     <div id="body-wrapper">
         <h1>About</h1>
         <p>Welcome to Marx. We love Karl Marx.</p>
+
+        <img src="/images/karl-marx.jpg" alt="karl marx seated and looking regal and intelligent" width="300px" height="400px">
     </div>
 @endsection
