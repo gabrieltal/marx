f0904926ddd8e51d3e8a19b8e037a6f952d4bd50
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-21 09:46:21 -0700

    Added a footer and called the app.css in the layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,10 +8,14 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="css/default.css" rel="stylesheet">
+        <link href="css/app.css" rel="stylesheet">
     </head>
     <body>
         @yield ('header')
         @yield ('content')
+
+        <footer><div class="d-flex align-items-center h-100">
+          <p>A Gabriel Talavera production.</p>
+        </div></footer>
     </body>
 </html>
