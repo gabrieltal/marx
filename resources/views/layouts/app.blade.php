017d64fbf2ce07db4bf7a54919973c648bd438d7
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-24 14:38:06 -0700

    Removed user name on navbar, just displays logout link instead and displays user name separately

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,18 +48,17 @@
                                     <a class="nav-link yellow-color" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }} <span class="caret"></span></a>
+                                <li class="nav-item">
+                                    <p class="mb-0 nav-link text-white">{{ Auth::user()->name }}</p>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link yellow-color" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
                                 </li>
                             @endguest
                         </ul>
