71c4ce30c40d2fb5e2d1b658ead159bcbc973cf3
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-24 10:28:38 -0700

    added controller validation for tags, store tags when submitted and added tags var for create

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\Post;
+use App\Tag;
 
 class PostsController extends Controller
 {
@@ -15,20 +16,29 @@ class PostsController extends Controller
 
     public function index()
     {
-        return view("posts.index", [
-            "posts" => Post::all()
-        ]);
+        if (request('tag')) {
+          $posts = Tag::where('name', request('tag'))->firstOrFail()->posts;
+        } else {
+          $posts = Post::all();
+        }
+
+        return view("posts.index", ["posts" => $posts]);
     }
 
     public function create()
     {
-        return view("posts.create");
+        return view("posts.create", ['tags' => Tag::all()]);
     }
 
     public function store()
     {
-        Post::create($this->validatePost());
+        $this->validatePost();
 
+        $post = new Post(request(['title', 'description', 'body']));
+        $post->user_id = 1;
+        $post->save();
+
+        $post->tags()->attach(request('tags'));
         return redirect('/posts');
     }
 
@@ -50,7 +60,8 @@ class PostsController extends Controller
         return request()->validate([
             "title" => "required",
             "description" => "required",
-            "body" => "required"
+            "body" => "required",
+            "tags" => "exists:tags,id"
         ]);
     }
 }
