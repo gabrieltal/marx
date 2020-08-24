487f5c7522e4d1f3cfccf75cdf810ac263d36e05
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-24 08:12:10 -0700

    Added models and association methods for Comment, Tag, Upvotes

--- app/Post.php
+++ app/Post.php
@@ -17,4 +17,19 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function upvotes()
+    {
+        return $this->hasMany(Upvote::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function upvotes()
+    {
+        return $this->hasMany(Upvote::class);
+    }
 }
