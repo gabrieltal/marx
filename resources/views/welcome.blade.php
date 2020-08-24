b9d7a30eb3d4de3374c9e3535fa776e3f759171b
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-24 13:41:11 -0700

    Used newly defined layout.app view throughout, updated nav and footer to be sticky and follow the bootstrap example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,8 @@
-@extends ('layout')
+@extends ('layouts.app')
 
 @section ('content')
     <div id="body-wrapper">
         <h2>Welcome!</h2>
         <a href="https://github.com/gabrieltal/marx" class="yellow-color">Github</a>
-    </div>
-    <div class="flex-center position-ref vh-100">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
     </div>
 @endsection
