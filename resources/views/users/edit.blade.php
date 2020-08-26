fb25694f4a549a6aa45a68bf5562e47e6d146ec7
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-26 13:51:00 -0700

    Added users edit page

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section ('content')
+    <div class="container-fluid">
+      <div class="row justify-content-center py-2">
+          <div class="col-lg-10">
+            <h1>Edit</h1>
+            <form action="/users" method="POST" class="form-container p-3">
+                @csrf
+                @method('PATCH')
+
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input class="form-control @error('username') is-invalid @enderror" type="text" name="username" id="username" value="{{ $user->username }}">
+                    @error('username')
+                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{ $user->name }}">
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="bio">Bio</label>
+                    <textarea class="form-control @error('bio') is-invalid @enderror" name="bio" rows="8" cols="80" id="bio">{{ $user->bio }}</textarea>
+                    @error('bio')
+                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </form>
+          </div>
+        </div>
+    </div>
+@endsection
