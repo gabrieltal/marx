303b3c71dcc47e4a12d6013f0ad66c70b259009b
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-26 13:50:53 -0700

    Added unfollow and isFollowing instance methods

--- app/User.php
+++ app/User.php
@@ -47,6 +47,16 @@ class User extends Authenticatable
         return $this->follows()->save($user);
     }
 
+    public function unfollow(User $user)
+    {
+        return $this->follows()->detach($user);
+    }
+
+    public function isFollowing(User $user)
+    {
+        return $this->follows()->where('following_user_id', $user->id)->exists();
+    }
+
     public function follows()
     {
         return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
