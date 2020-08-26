0c2e83d8ad7cf6d07e0ce9adcfc260435b4e3a17
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-26 08:03:44 -0700

    Added a displayName method for Users

--- app/User.php
+++ app/User.php
@@ -37,6 +37,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function displayName()
+    {
+        return "@$this->name";
+    }
+
+
     public function posts()
     {
         return $this->hasMany(Post::class);
