635ac70b01c48b6d2d54d816591f849948125db4
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-26 13:52:02 -0700

    Added user edit and update actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,4 +13,24 @@ class UsersController extends Controller
         return view("users.show", ["user" => $user]);
     }
 
+    public function edit()
+    {
+        return view("users.edit", ["user" => auth()->user()]);
+    }
+
+    public function update()
+    {
+        $user = auth()->user();
+        $user->update($this->validateUser());
+        return redirect($user->path())->with("message", "Updated your profile!");
+    }
+
+    protected function validateUser()
+    {
+        return request()->validate([
+            "username" => ['required', 'string', 'max:20', 'unique:users,username,'.auth()->user()->id.',id', 'alpha_dash'],
+            "name" => ['required', 'string', 'max:255'],
+            "bio" => ['required', 'string']
+        ]);
+    }
 }
