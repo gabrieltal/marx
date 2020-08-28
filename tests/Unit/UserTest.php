3b1439dce58b60fef49116c6c7a3c12144669378
Gabriel Talavera <gabrielvt6@gmail.com>
2020-08-28 09:46:23 -0700

    Added some tests for comraderies

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -16,4 +16,67 @@ class UserTest extends TestCase
 
         $this->assertInstanceOf(Collection::class, $user->posts);
     }
+
+    public function test_expects_has_follows()
+    {
+        $user = factory('App\User')->create();
+
+        $this->assertInstanceOf(Collection::class, $user->follows);
+    }
+
+    public function test_expects_has_post_comraderies()
+    {
+        $user = factory('App\User')->create();
+
+        $this->assertInstanceOf(Collection::class, $user->postComraderies);
+    }
+
+    public function test_expects_has_comment_comraderies()
+    {
+        $user = factory('App\User')->create();
+
+        $this->assertInstanceOf(Collection::class, $user->commentComraderies);
+    }
+
+    public function test_expects_has_comments()
+    {
+      $user = factory('App\User')->create();
+
+      $this->assertInstanceOf(Collection::class, $user->comments);
+    }
+
+    public function test_expects_giveComraderie_to_add_a_comraderie()
+    {
+      // Arrange
+      $user = $this->signIn();
+      $post = factory('App\Post')->create();
+
+      // Assume
+      $this->assertCount(0, $post->comraderies);
+
+      // Act
+      $user->giveComraderie($post);
+      $post->refresh();
+
+      // Assert
+      $this->assertCount(1, $post->comraderies);
+    }
+
+    public function test_expects_revokeComraderie_to_add_a_comraderie()
+    {
+      // Arrange
+      $user = $this->signIn();
+      $post = factory('App\Post')->create();
+      $user->giveComraderie($post);
+
+      // Assume
+      $this->assertCount(1, $post->comraderies);
+
+      // Act
+      $user->revokeComraderie($post);
+      $post->refresh();
+
+      // Assert
+      $this->assertCount(0, $post->comraderies);
+    }
 }
